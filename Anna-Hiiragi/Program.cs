ca632aec793b60dd6d3b7c988717bf0cf78c8505
AioiLight <info@aioilight.space>
2020-05-10 22:59:11 +0900

    using消す

--- Anna-Hiiragi/Program.cs
+++ Anna-Hiiragi/Program.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Timers;
-using AngleSharp.Html;
 using AngleSharp.Html.Parser;
 using Discord;
 using Discord.WebSocket;
