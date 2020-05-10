f3e079b3662373c9377c7964936251c703c9c04a
AioiLight <info@aioilight.space>
2020-05-10 19:50:57 +0900

    testコマンド

--- Anna-Hiiragi/Program.cs
+++ Anna-Hiiragi/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using AngleSharp.Html;
+using AngleSharp.Html.Parser;
 using Discord;
 using Discord.WebSocket;
 using Newtonsoft.Json;
@@ -18,6 +21,8 @@ namespace AioiLight.Anna_Hiiragi
                 return File.ReadAllText("token.txt", Encoding.UTF8);
             }
         }
+
+        static readonly HttpClient Client = new HttpClient();
 
         private static Config Config { get; set; }
 
@@ -53,12 +58,14 @@ namespace AioiLight.Anna_Hiiragi
                 {
                     await arg.Channel.SendMessageAsync($"{server.Name} はすでに初期設定が済んでいます！\n" +
                         $"リセットする場合は(権限のあるユーザーで)``!anna reset``");
+                    return;
                 }
                 else
                 {
                     Config.Servers.Add(new Server() { ServerID = server.Id, UserRole = roleID });
                     var role = server.Roles.Where(r => r.Id == roleID).First();
                     await arg.Channel.SendMessageAsync($"初期設定が完了しました。Bot を操作できる役職: {role.Name}");
+                    return;
                 }
             }
 
@@ -72,16 +79,19 @@ namespace AioiLight.Anna_Hiiragi
                     {
                         Config.Servers.Remove(cfg);
                         await arg.Channel.SendMessageAsync($"{server.Name} の設定を削除しました。");
+                        return;
                     }
                     else
                     {
                         await arg.Channel.SendMessageAsync($"{server.Name} の設定を削除できません。\n" +
                             $"役職: ${server.GetRole(cfg.UserRole).Name} 実行するユーザーに必要です。");
+                        return;
                     }
                 }
                 else
                 {
                     await arg.Channel.SendMessageAsync($"{server.Name} の設定はすでにありません。\n");
+                    return;
                 }
             }
 
@@ -115,11 +125,29 @@ namespace AioiLight.Anna_Hiiragi
                     {
                         if (param.Count() >= 2)
                         {
-                            await arg.Channel.SendMessageAsync($"テスト: \n" +
-                                $"動画URL: <{param[1]}>\n" +
-                                $"タイトル: {arg.Embeds.First().Title}\n" +
-                                $"ソース: {arg.Embeds.First().Provider.Value}");
-                            return;
+                            try
+                            {
+                                var response = await Client.GetAsync(param[1]);
+                                response.EnsureSuccessStatusCode();
+                                var responseBody = await response.Content.ReadAsStringAsync();
+
+                                var parser = new HtmlParser();
+                                var document = await parser.ParseDocumentAsync(responseBody);
+
+                                var h1 = document.GetElementsByTagName("h1");
+                                var title = h1.Last().TextContent.Trim();
+
+                                await arg.Channel.SendMessageAsync($"テスト:\n" +
+                                    $"URL: <{param[1]}>\n" +
+                                    $"タイトル: {title}");
+                                return;
+                            }
+                            catch (Exception e)
+                            {
+                                await arg.Channel.SendMessageAsync($"動画の解析ができませんでした……。\n" +
+                                    $"URL: <{param[1]}>");
+                                return;
+                            }
                         }
                     }
                     else if (param[0] == "save")
@@ -130,7 +158,6 @@ namespace AioiLight.Anna_Hiiragi
                         return;
                     }
                 }
-
             }
         }
     }
